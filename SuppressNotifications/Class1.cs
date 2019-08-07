5ff25eb3c826a7da6de76d423d76d6beed09b086
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-08-07 13:32:03 -0700

    Hello World

--- SuppressNotifications/Class1.cs
+++ SuppressNotifications/Class1.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Harmony;
 
 namespace SuppressNotifications
 {
-    public class Class1
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    internal class Class1
     {
+        private static void Postfix()
+        {
+            Debug.Log("Hellow World!");
+        }
     }
 }
