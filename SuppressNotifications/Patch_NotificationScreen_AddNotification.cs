74490960561b88924926ef02ecb1aa12b99b5749
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-08-10 15:43:11 -0700

    Removed logging and test code

--- SuppressNotifications/Patch_NotificationScreen_AddNotification.cs
+++ SuppressNotifications/Patch_NotificationScreen_AddNotification.cs
@@ -11,18 +11,7 @@ namespace SuppressNotifications
     {
         static void Prefix(Notification notification)
         {
-            
-            //Debug.Log(notification.titleText);
-            var comp = notification.Notifier.gameObject.GetComponent<NotificationsSuppressedComp>();
-            if (comp == null) { return; }
-            Debug.Log("ADD TO " + notification?.Notifier?.name + ": " + notification.titleText);
-
             notification.Notifier.gameObject.GetComponent<NotificationsSuppressedComp>()?.notifications.Add(notification);
-
-            foreach (var note in comp.notifications)
-            {
-                Debug.Log(note.titleText);
-            }
         }
     }
 
@@ -30,24 +19,8 @@ namespace SuppressNotifications
     class Patch_NotificationScreen_RemoveNotification
     {
         static void Prefix(Notification notification)
-        {
-            if (notification.Notifier == null)
-            {
-                //Debug.Log("NULL");
-                //Debug.Log(notification);
-                //Debug.Log(notification.titleText);
-                return;
-            }
-                
-            //Debug.Log("Remove");
-            //Debug.Log(notification.Notifier);
-            //Debug.Log(notification.Notifier.gameObject);
-            notification.Notifier.gameObject.GetComponent<NotificationsSuppressedComp>()?.notifications.Remove(notification);
+        {                
+            notification.Notifier?.gameObject.GetComponent<NotificationsSuppressedComp>()?.notifications.Remove(notification);
         }
-    }
-
-    public static class Util
-    {
-
     }
 }
