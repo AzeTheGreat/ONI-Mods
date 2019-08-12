a3c040f29b14d1293de1e4223c9329014f8b7524
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-08-11 20:02:55 -0700

    Publish

---
+++ SuppressNotifications/ModInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuppressNotifications
+{
+    class ModInfo
+    {
+        public static string Name = "Suppress Notifications";
+        public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
+    }
+}
