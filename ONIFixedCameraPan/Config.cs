476abec7cf4887b694a8ea0422c0575a03204492
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-08-25 23:40:21 -0700

    Works

---
+++ ONIFixedCameraPan/Config.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ONIFixedCameraPan
+{
+    [HarmonyPatch(typeof(CameraController), "OnPrefabInit")]
+    class Patch_CameraController_OnPrefabInit
+    {
+        private static float defaultMultiplier = 1f;
+
+        static void Postfix(ref float ___keyPanningSpeed)
+        {
+            ___keyPanningSpeed *= GetMultiplier();
+        }
+
+        private static float GetMultiplier()
+        {
+            Config config = ReadConfig();
+
+            if (config == null)
+            {
+                CreateConfig();
+                return defaultMultiplier;
+            }
+            else
+            {
+                return config.multiplier;
+            }
+        }
+
+        private static Config ReadConfig()
+        {
+            Config config = null;
+            string directory = Path.GetDirectoryName(
+                new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            var configPath = Path.Combine(directory, "config");
+
+            try
+            {
+                using (var r = new StreamReader(configPath))
+                {
+                    var json = r.ReadToEnd();
+                    config = JsonConvert.DeserializeObject<Config>(json);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Error reading config file with exception: " + e.Message);
+                return config;
+            }
+
+            return config;
+        }
+
+        private static void CreateConfig()
+        {
+            Config config = new Config();
+            config.multiplier = defaultMultiplier;
+
+            string directory = Path.GetDirectoryName(
+                new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            var configPath = Path.Combine(directory, "config");
+
+            using (var r = new StreamWriter(configPath))
+            {
+                var serialized = JsonConvert.SerializeObject(config, Formatting.Indented);
+                r.Write(serialized);
+            }
+        }
+    }
+
+    class Config
+    {
+        public float multiplier;
+    }
+}
---
+++ ONIFixedCameraPan/Patch_CameraController_NormalCamUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Harmony;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+
+namespace ONIFixedCameraPan
+{
+    [HarmonyPatch(typeof(CameraController), "NormalCamUpdate")]
+    public class Patch_CameraController_NormalCamUpdate
+    {
+        private static float maxDeltaTime = 0.1f;
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            FieldInfo targetFieldInfo = AccessTools.Field(typeof(CameraController), nameof(CameraController.keyPanningSpeed));
+
+            foreach (CodeInstruction i in instructions)
+            {
+                if (i.opcode == OpCodes.Ldfld && i.operand == targetFieldInfo)
+                {
+                    yield return i;
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Call,
+                        AccessTools.Method(typeof(Patch_CameraController_NormalCamUpdate), nameof(Patch_CameraController_NormalCamUpdate.GetScalar)));
+                    yield return new CodeInstruction(OpCodes.Mul);
+                }
+
+                else
+                    yield return i;
+            }
+        }
+
+        private static float GetScalar(float unscaledDeltaTime)
+        {
+            return Mathf.Clamp(unscaledDeltaTime, 0f, maxDeltaTime);
+        }
+    }
+}
