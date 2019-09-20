e54f2b03678154151fcde24f5c4adb225c394f24
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-09-20 08:36:06 -0700

    Created ALib and setup ILMerge

--- FixedCameraPan/Config.cs
+++ FixedCameraPan/Config.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Harmony;
-using System.IO;
+﻿using Harmony;
 using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace ONIFixedCameraPan
 {
