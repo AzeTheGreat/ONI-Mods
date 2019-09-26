21bb763e5d0762b8a933be46059938e5df3760de
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-09-25 23:19:18 -0700

    Added mod: No Research Alerts

---
+++ NoResearchAlerts/NoResearchAlerts_Patch.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace NoResearchAlerts
+{
+    [HarmonyPatch(typeof(PlanCategoryNotifications), nameof(PlanCategoryNotifications.ToggleAttention))]
+    public class NoResearchAlerts_Patch
+    {
+        static void Prefix(ref bool active)
+        {
+            active = false;
+        }
+    }
+}
