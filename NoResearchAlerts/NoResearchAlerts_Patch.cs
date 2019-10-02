1f43e23c4e300143de99a2535fac851bd2b46546
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-10-01 21:29:41 -0700

    Version logging for all mods

--- NoResearchAlerts/NoResearchAlerts_Patch.cs
+++ NoResearchAlerts/NoResearchAlerts_Patch.cs
@@ -1,10 +1,16 @@
 ﻿using Harmony;
+using PeterHan.PLib;
 
 namespace NoResearchAlerts
 {
     [HarmonyPatch(typeof(PlanCategoryNotifications), nameof(PlanCategoryNotifications.ToggleAttention))]
     public class NoResearchAlerts_Patch
     {
+        public static void OnLoad()
+        {
+            PUtil.LogModInit();
+        }
+
         static void Prefix(ref bool active)
         {
             active = false;
--- SuppressNotifications/Util/ModInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SuppressNotifications
-{
-    class ModInfo
-    {
-        public static string Name = "Suppress Notifications";
-        public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
-    }
-}
