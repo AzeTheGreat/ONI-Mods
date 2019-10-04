e12126b63569f05a4390b157b2b7e454b115596a
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-10-04 13:25:24 -0700

    Cleaned code and removed casting nonsense

--- BetterLogicPortDisplay/GateColorOutput_Patch.cs
+++ BetterLogicPortDisplay/GateColorOutput_Patch.cs
@@ -11,26 +11,11 @@ namespace BetterLogicPortDisplay
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
         {
             bool flag1 = false;
-            //bool flag2 = false;
-            //bool flag3 = false;
-
-            //object localCircuitManager = null;
-            //object localCircuitNetwork = null;
 
             foreach (CodeInstruction i in instructions)
             {
                 if (i.opcode == OpCodes.Cgt)
                     flag1 = true;
-
-                //if (i.opcode == OpCodes.Ldfld && i.operand == AccessTools.Field(
-                //    typeof(Game),
-                //    nameof(Game.logicCircuitManager)))
-                //    flag2 = true;
-
-                //if (i.opcode == OpCodes.Callvirt && i.operand == AccessTools.Method(
-                //    typeof(LogicCircuitManager),
-                //    nameof(LogicCircuitManager.GetNetworkForCell)))
-                //    flag3 = true;
 
                 if (i.opcode == OpCodes.Stloc_S && flag1)
                 {
@@ -50,9 +35,8 @@ namespace BetterLogicPortDisplay
                     yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(
                         AccessTools.TypeByName("OverlayModes+Logic+UIInfo"),
                         "cell"));
-                    yield return new CodeInstruction(OpCodes.Box, typeof(int));
                     yield return new CodeInstruction(OpCodes.Ldloc_S, 6);
-                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(
                         typeof(GateOutputColor_Patch),
                         nameof(GateOutputColor_Patch.Helper)));
 
@@ -64,28 +48,16 @@ namespace BetterLogicPortDisplay
                 }
                 else
                     yield return i;
-
-                //if(i.opcode == OpCodes.Ldloca_S && flag2)
-                //{
-                //    localCircuitManager = i.operand;
-                //    flag2 = false;
-                //}
-
-                //if(i.opcode == OpCodes.Stloc_S && flag3)
-                //{
-                //    localCircuitNetwork = i.operand;
-                //    flag3 = false;
-                //}
             }
         }
 
-        public static bool Helper(object cell, LogicCircuitNetwork networkForCell)
+        public static bool Helper(int cell, LogicCircuitNetwork networkForCell)
         {
             foreach (var sender in networkForCell.Senders)
             {
-                if (sender.GetLogicCell() == (int)cell)
+                if (sender.GetLogicCell() == cell)
                 {
-                    if(sender.GetLogicValue() <= 0)
+                    if (sender.GetLogicValue() <= 0)
                         return false;
 
                     return true;
