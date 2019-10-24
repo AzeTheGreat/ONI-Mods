c2c6c1ca2d0846008b59717402c9d050931f8b92
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-10-24 10:59:23 -0700

    Sufficient Oxygen Generation initial

---
+++ SufficientOxygenGeneration/Options.cs
@@ -0,0 +1,44 @@
+﻿using Harmony;
+using Newtonsoft.Json;
+using PeterHan.PLib;
+using PeterHan.PLib.Options;
+
+namespace SufficientOxygenGeneration
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Options
+    {
+        [Option("Notification Threshold", "Set to net oxygen production in kg below which you'd like to be notified.")]
+        [Limit(-99999f, 0.0001f)]
+        [JsonProperty]
+        public float OxygenThreshold { get; set; }
+
+        public Options()
+        {
+            OxygenThreshold = -10f;
+        }
+
+        public static void ReadSettings()
+        {
+            options = POptions.ReadSettings<Options>() ?? new Options();
+        }
+
+        public static Options options;
+
+        public static void OnLoad()
+        {
+            PUtil.LogModInit();
+            options = new Options();
+            POptions.RegisterOptions(typeof(Options));
+        }
+
+        [HarmonyPatch(typeof(Game), "OnPrefabInit")]
+        public static class Game_OnPrefabInit_Patch
+        {
+            internal static void Prefix()
+            {
+                ReadSettings();
+            }
+        }
+    }
+}
---
+++ SufficientOxygenGeneration/Threshold_Patch.cs
@@ -0,0 +1,32 @@
+﻿using Harmony;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SufficientOxygenGeneration
+{
+    [HarmonyPatch(typeof(Tutorial), "SufficientOxygenLastCycleAndThisCycle")]
+    public class Threshold_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            Options.ReadSettings();
+            bool second = false;
+
+            foreach (CodeInstruction i in instructions)
+            {
+                if(i.opcode == OpCodes.Ldc_R4)
+                {
+                    if (second)
+                        yield return new CodeInstruction(OpCodes.Ldc_R4, Options.options.OxygenThreshold);
+                    else
+                    {
+                        second = true;
+                        yield return i;
+                    } 
+                }
+                else
+                    yield return i;
+            }
+        }
+    }
+}
