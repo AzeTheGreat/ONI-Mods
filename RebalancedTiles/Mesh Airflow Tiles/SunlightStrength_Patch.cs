ef0ea14cdab33299c597c570b5fd78935a39d633
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-10-26 22:32:40 -0700

    Fixed bug where mesh tiles incorrectly affected standard light

--- RebalancedTiles/Mesh Airflow Tiles/SunlightStrength_Patch.cs
+++ RebalancedTiles/Mesh Airflow Tiles/SunlightStrength_Patch.cs
@@ -1,15 +1,20 @@
 ﻿using Harmony;
+using System;
 
 namespace RebalancedTiles.Mesh_Airflow_Tiles
 {
     [HarmonyPatch(typeof(Grid.LightIntensityIndexer), "get_Item")]
     class SunlightStrength_Patch
     {
-        static void Postfix(int i, ref int __result)
+        static unsafe bool Prefix(int i, ref int __result)
         {
-            __result -= (int)((float)SunlightModifierGrid.sunlightModifiers[i] / 255f * Game.Instance.currentSunlightIntensity);
-            if (__result < 0)
-                __result = 0;
+            int adjustedExposure = Math.Max(0, Grid.exposedToSunlight[i] - SunlightModifierGrid.sunlightModifiers[i]);
+            int sunlight = (int)(adjustedExposure / 255f * Game.Instance.currentSunlightIntensity);
+
+            int light = Grid.LightCount[i];
+
+            __result = sunlight + light;
+            return false;
         }
     }
 }
