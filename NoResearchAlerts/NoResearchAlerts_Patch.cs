0b3342744b7fa628f543b6bc5b3043c18d547144
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-11-07 16:03:24 -0700

    Show research alerts but clear after category is closed

--- NoResearchAlerts/NoResearchAlerts_Patch.cs
+++ NoResearchAlerts/NoResearchAlerts_Patch.cs
@@ -1,19 +1,40 @@
 ﻿using Harmony;
-using PeterHan.PLib;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NoResearchAlerts
 {
     [HarmonyPatch(typeof(PlanCategoryNotifications), nameof(PlanCategoryNotifications.ToggleAttention))]
     public class NoResearchAlerts_Patch
     {
-        public static void OnLoad()
-        {
-            PUtil.LogModInit();
-        }
+        static bool Prepare() => Options.Opts.AlertMode == Options.Mode.None;
 
         static void Prefix(ref bool active)
         {
             active = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(PlanScreen), "ClearButtons")]
+    public class FastClearAlerts_Patch
+    {
+        static bool Prepare() => Options.Opts.AlertMode == Options.Mode.FastClear;
+
+        static void Prefix(Dictionary<BuildingDef, KToggle> ___ActiveToggles, Dictionary<Tag, HashedString> ___tagCategoryMap)
+        {
+            if (___ActiveToggles == null || ___ActiveToggles.Count == 0)
+                return;
+
+            var item = ___ActiveToggles.FirstOrDefault();
+            HashedString category = ___tagCategoryMap[item.Key.Tag];
+            object[] parameters = new object[] { category, null };
+
+            AccessTools.Method(typeof(PlanScreen), "GetToggleEntryForCategory").Invoke(PlanScreen.Instance, parameters);
+            List<Tag> pendingResearchAttentions = parameters[1].GetType().GetField("pendingResearchAttentions").GetValue(parameters[1]) as List<Tag>;
+            KIconToggleMenu.ToggleInfo toggleInfo = parameters[1].GetType().GetField("toggleInfo").GetValue(parameters[1]) as KIconToggleMenu.ToggleInfo;
+
+            pendingResearchAttentions.Clear();
+            toggleInfo.toggle.GetComponent<PlanCategoryNotifications>().ToggleAttention(false);
         }
     }
 }
---
+++ NoResearchAlerts/Options.cs
@@ -0,0 +1,34 @@
+﻿using AzeLib;
+using Newtonsoft.Json;
+using PeterHan.PLib;
+using PeterHan.PLib.Options;
+
+namespace NoResearchAlerts
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    [RestartRequired]
+    class Options : BaseOptions<Options>
+    {
+        public enum Mode
+        {
+            [Option("No Alerts", "Research alerts will never show")]
+            None,
+            [Option("Fast Clear", "Research alerts will be shown, but are cleared after looking at the category.")]
+            FastClear
+        }
+
+        [Option("Mode", "The mode to use.")]
+        [JsonProperty]
+        public Mode AlertMode { get; set; }
+
+        public Options()
+        {
+            AlertMode = Mode.None;
+        }
+
+        public static void OnLoad()
+        {
+            Load();
+        }
+    }
+}
