bed644c786cfe486707d0b8fa318dc87296d9dc6
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-11-13 23:37:14 -0700

    Restructured to manipulate info cards after structure is determined

--- BetterInfoCards/EditWidgets_Patch.cs
+++ BetterInfoCards/EditWidgets_Patch.cs
@@ -44,28 +44,92 @@ namespace BetterInfoCards
 
         private static void ArrangeInfoCards()
         {
+            //// No point in arranging.
+            //if (infoCards.Count < 4)
+            //    return;
+
+            //// Determine column numbers
+            //float totalLength = infoCards.Sum(x => x.shadowBar.rect.rect.height);
+            //float averageWidth = infoCards.Average(x => x.shadowBar.rect.rect.width);
+
+            //int columns = Mathf.CeilToInt(Mathf.Sqrt(targetAspectRatio * totalLength / averageWidth));
+            //float lengthPerColumn = totalLength / columns;
+
+            //// Build "grid"
+            //float currentColLength = 0f;
+            //float currentOffsetX = 0f;
+            //float currentOffsetY = 0f;
+            //int colStartIndex = 0;
+            //for (int i = 0; i < infoCards.Count; i++)
+            //{
+            //    infoCards[i].Translate(currentOffsetX, currentOffsetY);
+            //    currentColLength += infoCards[i].shadowBar.rect.rect.height;
+
+            //    if (currentColLength > lengthPerColumn && i < infoCards.Count - 1)
+            //    {
+            //        currentColLength = 0f;
+            //        currentOffsetX += infoCards.GetRange(colStartIndex, i-colStartIndex+1).Max(x => x.shadowBar.rect.rect.width) + shadowBarSpacingX;
+            //        currentOffsetY = infoCards[0].shadowBar.rect.anchoredPosition.y - infoCards[i+1].shadowBar.rect.anchoredPosition.y;
+            //        colStartIndex = i;
+            //    }
+            //}
+
             float minY = -infoCards[0].shadowBar.rect.gameObject.GetComponentInParent<Canvas>().pixelRect.height / infoCards[0].shadowBar.rect.gameObject.GetComponentInParent<Canvas>().scaleFactor;
 
             float offsetX = 0f;
-            float offsetY = 0f;
-            float maxXInCol = 0f;
-            int card = 0;
+
+            var gridInfo = new List<ColumnInfo>() { };
+            var colInfo = new ColumnInfo
+            {
+                offsetX = 0f,
+                offsetY = 0f,
+                infoCards = new List<InfoCard>()
+            };
 
             foreach (var infoCard in infoCards)
             {
-                if (infoCard.shadowBar.rect.anchoredPosition.y - infoCard.shadowBar.rect.rect.height + offsetY < minY)
+                if (infoCard.YMin + colInfo.offsetY < minY)
                 {
-                    offsetX += maxXInCol + shadowBarSpacingX;
-                    offsetY = infoCards[0].shadowBar.rect.anchoredPosition.y - infoCard.shadowBar.rect.anchoredPosition.y;
-                    maxXInCol = 0f;
+                    offsetX += colInfo.maxXInCol + shadowBarSpacingX;
+
+                    colInfo.isLeftCol = true;
+
+                    gridInfo.Add(colInfo);
+                    colInfo = new ColumnInfo
+                    {
+                        offsetX = offsetX,
+                        offsetY = infoCards[0].YMax - infoCard.YMax,
+                        infoCards = new List<InfoCard>()
+                    };
                 }
 
-                if (infoCard.shadowBar.rect.rect.width > maxXInCol)
-                    maxXInCol = infoCard.shadowBar.rect.rect.width;
+                if (infoCard.Width > colInfo.maxXInCol)
+                    colInfo.maxXInCol = infoCard.Width;
 
-                infoCard.Translate(offsetX, offsetY);
-                card++;
+                colInfo.infoCards.Add(infoCard);
             }
+
+            gridInfo.Add(colInfo);
+
+            foreach (var info in gridInfo)
+            {
+                foreach (var card in info.infoCards)
+                {
+                    card.Translate(info.offsetX, info.offsetY);
+
+                    if (info.isLeftCol)
+                        card.shadowBar.rect.sizeDelta = new Vector2(info.maxXInCol, card.shadowBar.rect.sizeDelta.y);
+                }
+            }
+        }
+
+        private class ColumnInfo
+        {
+            public float offsetX;
+            public float offsetY;
+            public List<InfoCard> infoCards;
+            public float maxXInCol = 0f;
+            public bool isLeftCol = false;
         }
 
         private static bool RectWithin(Entry main, Entry sub)
@@ -80,6 +144,10 @@ namespace BetterInfoCards
             public List<Entry> iconWidgets;
             public List<Entry> textWidgets;
             public Entry selectBorder;
+
+            public float Width { get { return shadowBar.rect.rect.width; } }
+            public float YMax { get { return shadowBar.rect.anchoredPosition.y; } }
+            public float YMin { get { return YMax - shadowBar.rect.rect.height; } }
 
             public InfoCard(Entry shadowBar, ref int iconIndex, ref int textIndex)
             {
