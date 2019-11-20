473aaa856eea3bff1eb939b149dd4048e847f053
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-11-20 10:53:25 -0700

    Patched mass strings so elements are structured the same as other objects

---
+++ BetterInfoCards/MassStringFix_Patch.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace BetterInfoCards
+{
+    [HarmonyPatch(typeof(WorldInspector), nameof(WorldInspector.MassStringsReadOnly))]
+    class MassStringFix_Patch
+    {
+        // Functions even on cached, fix later
+        static void Postfix(string[] __result)
+        {
+            __result[0] = __result[0] + __result[1] + __result[2] + __result[3];
+            __result[1] = __result[2] = __result[3] = string.Empty;
+        }
+    }
+
+    [HarmonyPatch(typeof(HoverTextDrawer), nameof(HoverTextDrawer.DrawText), new System.Type[] { typeof(string), typeof(TextStyleSetting), typeof(Color), typeof(bool)})]
+    class DrawEmptyStringFix_Patch
+    {
+        static bool Prefix(string text)
+        {
+            if (text == string.Empty)
+                return false;
+
+            return true;
+        }
+    }
+}
