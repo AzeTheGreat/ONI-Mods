0769bca5c03b3a7dd9d7679cc8b51733ed3261b3
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-11-22 11:16:02 -0700

    O2 ratio fix

--- SufficientOxygenGeneration/Threshold_Patch.cs
+++ SufficientOxygenGeneration/Threshold_Patch.cs
@@ -15,14 +15,14 @@ namespace SufficientOxygenGeneration
 
             ReportManager.ReportEntry entryYest = ReportManager.Instance.YesterdaysReport.GetEntry(ReportManager.ReportType.OxygenCreated);
             ReportManager.ReportEntry entryToday = ReportManager.Instance.TodaysReport.GetEntry(ReportManager.ReportType.OxygenCreated);
-
+            
             switch (Options.Opts.Mode)
             {
                 case Options.OxygenThresholMode.Constant:
                     __result = entryToday.Net > -Options.Opts.ConstantThreshold || entryYest.Net > 0.0001f;
                     break;
                 case Options.OxygenThresholMode.Ratio:
-                    __result = ((entryToday.Positive / entryToday.Negative) > Options.Opts.RatioThreshold || entryYest.Net > 0.0001f) && GameClock.Instance.GetTimeSinceStartOfCycle() > Options.Opts.TimeDelay;
+                    __result = ((entryToday.Positive / -entryToday.Negative) > Options.Opts.RatioThreshold || entryYest.Net > 0.0001f) && GameClock.Instance.GetTimeSinceStartOfCycle() > Options.Opts.TimeDelay;
                     break;
                 default:
                     __result = true;
