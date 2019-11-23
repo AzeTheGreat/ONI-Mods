2ec0368285f3773f1bc3f45c6255f6ba420a2495
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2019-11-23 13:18:06 -0700

    fixed(?) rare frame-perfect crash

---
+++ BetterInfoCards/Hits/UpdateHitsBefore_Patch.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace BetterInfoCards.Hits
+{
+    [HarmonyPatch(typeof(InterfaceTool), nameof(InterfaceTool.LateUpdate))]
+    class UpdateHitsBefore_Patch
+    {
+        const int updateCodesCount = 4;
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            MethodInfo updateTarget = AccessTools.Method(typeof(InterfaceTool), "UpdateHoverElements");
+            MethodInfo insertTarget = AccessTools.Method(typeof(InterfaceTool), "GetSelectablesUnderCursor");
+
+            int updateIndex = codes.FindIndex(x => x.opcode == OpCodes.Call && x.operand == updateTarget) - 3;
+            int insertIndex = codes.FindIndex(x => x.opcode == OpCodes.Call && x.operand == insertTarget) + 1;
+
+            codes.InsertRange(insertIndex, codes.GetRange(updateIndex, updateCodesCount));
+            codes.RemoveRange(updateIndex + updateCodesCount, updateCodesCount);
+
+            return codes;
+        }
+    }
+}
