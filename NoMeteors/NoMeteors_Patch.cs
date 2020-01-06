3deb5a54808aed5ac3e217b6be86d11f40f8644b
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2020-01-06 16:35:30 -0700

    Mod to remove meteors

---
+++ NoMeteors/NoMeteors_Patch.cs
@@ -0,0 +1,10 @@
+﻿using Harmony;
+
+namespace NoMeteors
+{
+    [HarmonyPatch(typeof(SeasonManager), "SpawnBombard")]
+    public class NoMeteors_Patch
+    {
+        static bool Prefix() => false;
+    }
+}
