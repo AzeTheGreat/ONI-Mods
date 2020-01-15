b87f72f64c6a513337112a59e4ce7a33b1afed55
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2020-01-15 00:01:07 -0700

    Silly mod

---
+++ src/DrinkTeaNotCoffee/STRINGS.cs
@@ -0,0 +1,18 @@
+﻿using STRINGS;
+
+namespace DrinkTeaNotCoffee
+{
+    class STRINGS
+    {
+        public class TEAMACHINE
+        {
+            public static LocString NAME = UI.FormatAsLink("Tea Station", "ESPRESSOMACHINE");
+            public static LocString DESC = "A cup of tea helps Duplicants relax after a long day.";
+        }
+
+        public class TEA
+        {
+            public static LocString NAME = "Drank Tea";
+        }
+    }
+}
---
+++ src/DrinkTeaNotCoffee/Tea.cs
@@ -0,0 +1,18 @@
+﻿using PeterHan.PLib.Datafiles;
+using STRINGS;
+
+namespace DrinkTeaNotCoffee
+{
+    public class Tea
+    {
+        public static void OnLoad()
+        {
+            PLocalization.Register();
+
+            BUILDINGS.PREFABS.ESPRESSOMACHINE.NAME = STRINGS.TEAMACHINE.NAME;
+            BUILDINGS.PREFABS.ESPRESSOMACHINE.DESC = STRINGS.TEAMACHINE.DESC;
+
+            DUPLICANTS.MODIFIERS.ESPRESSO.NAME = STRINGS.TEA.NAME;
+        }
+    }
+}
