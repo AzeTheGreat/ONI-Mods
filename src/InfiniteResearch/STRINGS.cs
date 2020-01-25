19abe02bb9eb62c5486e97c6b7a324fa88676cc3
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2020-01-25 12:48:07 -0700

    Added custom status item displaying the skill range

---
+++ src/InfiniteResearch/STRINGS.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+
+namespace InfiniteResearch
+{
+    [HarmonyPatch(typeof(GlobalAssets), "Awake")]
+    public class BUILDING
+    {
+        static void Postfix() => LocString.CreateLocStringKeys(typeof(BUILDING));
+
+        public class STATUSITEMS
+        {
+            public class REQUIRESATTRIBUTERANGE
+            {
+                public static LocString NAME = "Attribute-Required Operation";
+
+                public static LocString TOOLTIP = "Only duplicants with a Science Attribute between {Attributes} can learn from this building.";
+            }
+        }
+    }
+}
