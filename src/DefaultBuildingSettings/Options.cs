845659be7e7203087db43b5625c4dfa9ce5d0724
AzeTheGreat <36392269+AzeTheGreat@users.noreply.github.com>
2020-02-01 18:58:15 -0700

    Simple mod to change the default settings on some buildings

---
+++ src/DefaultBuildingSettings/Options.cs
@@ -0,0 +1,29 @@
+﻿using AzeLib;
+using Newtonsoft.Json;
+using PeterHan.PLib;
+
+namespace DefaultBuildingSettings
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Options : BaseOptions<Options>
+    {
+        [Option("Vacancy Only", "If checked, atmo and jet suit checkpoint are set to Vacancy Only by default.")]
+        [JsonProperty]
+        public bool VacancyOnly { get; set; }
+
+        [Option("SweepOnly", "If checked, storage receptacles are set to sweep only by default.")]
+        [JsonProperty]
+        public bool SweepOnly { get; set; }
+
+        public Options()
+        {
+            VacancyOnly = true;
+            SweepOnly = true;
+        }
+
+        public static void OnLoad()
+        {
+            Load();
+        }
+    }
+}
---
+++ src/DefaultBuildingSettings/Patches.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace DefaultBuildingSettings
+{
+    // Not an ideal approach, but the best that can be done given the game's current structure
+    // TODO: Check periodically and see if the configs can just be edited as normal.
+    [HarmonyPatch(typeof(BuildingDef), nameof(BuildingDef.Build))]
+    public class Patches
+    {
+        static void Postfix(GameObject __result)
+        {
+            var suitMarker = __result.GetComponent<SuitMarker>();
+            if(suitMarker)
+                Traverse.Create(suitMarker).Field("onlyTraverseIfUnequipAvailable").SetValue(Options.Opts.VacancyOnly);
+
+            var storage = __result.GetComponent<Storage>();
+            if (storage)
+                Traverse.Create(storage).Field("onlyFetchMarkedItems").SetValue(Options.Opts.SweepOnly);
+        }
+    }
+}
