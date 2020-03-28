d634e299bcbb177b84573e73eed9914f1aab2d8e
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2020-03-28 13:51:26 -0700

    Cleaner solution for duplicate temperatures.  Fixes compat with Thermal Tooltips.

--- src/BetterInfoCards/Tweaks/PreventDuplicateTempText.cs
+++ src/BetterInfoCards/Tweaks/PreventDuplicateTempText.cs
@@ -1,33 +1,14 @@
-﻿using AzeLib.Extensions;
+﻿using Database;
 using Harmony;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
 
 namespace BetterInfoCards.Tweaks
 {
-    [HarmonyPatch(typeof(SelectToolHoverTextCard), nameof(SelectToolHoverTextCard.UpdateHoverElements))]
+    [HarmonyPatch(typeof(MiscStatusItems), "CreateStatusItems")]
     class PreventDuplicateTempText
     {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        static void Postfix(MiscStatusItems __instance)
         {
-            var targetMethod = AccessTools.Method(typeof(UnityEngine.Component), "GetComponent").MakeGenericMethod(typeof(Constructable));
-
-            var ins = instructions.ToList();
-
-            int targetI = ins.FindIndex(x => x.OperandIs(targetMethod)) + 2;
-            ins.InsertRange(targetI, insert());
-
-            IEnumerable<CodeInstruction> insert()
-            {
-                var lastLocalSelectable = instructions.FindPrior(ins[targetI], x => x.IsLocalOfType(typeof(KSelectable))).operand;
-                yield return new CodeInstruction(OpCodes.Ldloc_S, lastLocalSelectable);
-                yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PreventDuplicateTempText), nameof(PreventDuplicateTempText.PreventDuplcate)));
-            }
-
-            return ins;
+            __instance.OreTemp.status_overlays &= ~(int)StatusItem.StatusItemOverlays.Temperature;
         }
-
-        private static bool PreventDuplcate(bool isConstructable, KSelectable selectable) => isConstructable || selectable.GetComponent<ElementChunk>();
     }
 }
