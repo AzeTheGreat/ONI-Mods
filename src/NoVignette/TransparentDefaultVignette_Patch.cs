49c46fd091690e6b2527288f2b4a58219e576db5
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2020-07-05 11:53:49 -0700

    New mod: Remove vignette

---
+++ src/NoVignette/TransparentDefaultVignette_Patch.cs
@@ -0,0 +1,12 @@
+﻿using Harmony;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NoVignette
+{
+    [HarmonyPatch(typeof(Vignette), "Awake")]
+    public class TransparentDefaultVignette_Patch
+    {
+        static void Prefix(ref Image ___image) => ___image.color = Color.clear;
+    }
+}
