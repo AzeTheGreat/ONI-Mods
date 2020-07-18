976a72cc0ede68b0068efe7e6ba5f6f3a1948e59
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2020-07-17 17:33:35 -0700

    Hide exclamation marks instead of just the text on the watermark.

--- src/CleanHUD/Watermark_Patch.cs
+++ src/CleanHUD/Watermark_Patch.cs
@@ -9,7 +9,7 @@ namespace CleanHUD
         {
             // Hide only if in game so that it still shows on the main menu.
             if (Options.Opts.IsWatermarkDisabled && Game.Instance)
-                __instance.textDisplay.SetText(string.Empty);
+                __instance.Show(false);
         }
     }
 }
