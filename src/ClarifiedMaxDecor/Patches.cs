c4f525f96331b9d03e0c03dffecdfe9782dbed72
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2020-07-17 22:27:34 -0700

    New mod: clarify how max decor behaves in game.

---
+++ src/ClarifiedMaxDecor/Patches.cs
@@ -0,0 +1,35 @@
+﻿using AzeLib.Extensions;
+using Harmony;
+using Klei.AI;
+using STRINGS;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace ClarifiedMaxDecor
+{
+    [HarmonyPatch(typeof(SelectToolHoverTextCard), nameof(SelectToolHoverTextCard.UpdateHoverElements))]
+    class RemoveCapFromCard
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var targetMethod = AccessTools.Method(typeof(GameUtil), nameof(GameUtil.GetFormattedDecor));
+            var codes = instructions.ToList();
+
+            var targetIndex = codes.FindLastIndex(codes.FindIndex(i => i.OperandIs(targetMethod)), i => i.OpCodeIs(OpCodes.Ldc_I4_1));
+            codes[targetIndex] = new CodeInstruction(OpCodes.Ldc_I4_0);
+
+            return codes.AsEnumerable();
+        }
+    }
+
+    [HarmonyPatch(typeof(DecorDisplayer), nameof(DecorDisplayer.GetTooltip))]
+    class AddCapToVitalsTooltip
+    {
+        static void Postfix(ref string __result, AmountInstance instance)
+        {
+            var format = instance.gameObject.GetSMI<DecorMonitor.Instance>().GetYesterdaysAverageDecor() > DecorMonitor.MAXIMUM_DECOR_VALUE ? UI.OVERLAYS.DECOR.MAXIMUM_DECOR : UI.OVERLAYS.DECOR.VALUE;
+            __result = string.Format(STRINGS.DUPLICANTS.ATTRIBUTES.QUALITYOFLIFE.DESC_FORMAT, __result, string.Format(format, "", DecorMonitor.MAXIMUM_DECOR_VALUE));
+        }
+    }
+}
