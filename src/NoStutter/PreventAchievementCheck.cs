ad7cd64eedcc3e12d60b33a273d28078ef9f9d2e
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2020-12-25 15:04:36 -0700

    New mod to fix the stutter when too much world is revealed without achieving Outdoor Renovations

---
+++ src/NoStutter/PreventAchievementCheck.cs
@@ -0,0 +1,27 @@
+﻿using Database;
+using Harmony;
+
+namespace NoStutter
+{
+    [HarmonyPatch(typeof(BuildOutsideStartBiome), nameof(BuildOutsideStartBiome.Success))]
+    class PreventAchievementCheck
+    {
+        static bool Prefix(ref bool __result)
+        {
+            if (Options.Opts.OnlyInDebug && !Game.Instance.debugWasUsed)
+                return true;
+
+            __result = Options.Opts.Mode switch
+            {
+                Options.DisableMode.Prevent => false,
+                Options.DisableMode.InstantSuccess => true,
+                _ => false
+            };
+
+            if(__result == true)
+                Game.Instance.unlocks.Unlock("buildoutsidestartingbiome");
+
+            return false;
+        }
+    }
+}
