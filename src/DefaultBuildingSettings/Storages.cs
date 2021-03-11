abbb18062946d1a24473519f90d5477ac9596dd8
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2021-03-10 18:38:47 -0700

    Fix issue where certain buildings could incorrectly be set to sweep only.

--- src/DefaultBuildingSettings/Storages.cs
+++ src/DefaultBuildingSettings/Storages.cs
@@ -8,10 +8,14 @@ namespace DefaultBuildingSettings
         [HarmonyPatch(typeof(TreeFilterable), "OnPrefabInit")]
         private class SweepOnly_Patch
         {
-            static void Postfix(Storage ___storage)
+            static void Postfix(TreeFilterable __instance)
             {
-                if (Options.Opts.SweepOnly && ___storage.allowSettingOnlyFetchMarkedItems)
-                    ___storage.SetOnlyFetchMarkedItems(true);
+                // TreeFilterableSideScreen.IsValidForTarget manually blacklists FlatTagFilterables
+                // Without this, rocket oxidizer tanks get set to sweep only despite not having the UI
+                // Must use reflection since FlatTagFilterable doesn't exist in vanilla currently
+                if (Options.Opts.SweepOnly && __instance.GetComponent(nameof(FlatTagFilterable)) == null && __instance.storage.allowSettingOnlyFetchMarkedItems)
+                    __instance.storage.SetOnlyFetchMarkedItems(true);
+                    
             }
         }
 
