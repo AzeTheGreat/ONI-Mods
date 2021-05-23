8be97658106b8d92e623efe0ff1d71df9ad08ef8
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2021-05-23 15:36:48 -0700

    Simplify patches to change Info Card appearance
    Removed unnecessary null checks and refs from font size patch.

--- src/BetterInfoCards/Tweaks/ChangeInfoCardAppearance.cs
+++ src/BetterInfoCards/Tweaks/ChangeInfoCardAppearance.cs
@@ -8,42 +8,24 @@ namespace BetterInfoCards
     class FontSizeDecrease_Patch
     {
         private static readonly int fontSizeDecrease = Options.Opts.InfoCardSize.fontSizeDecrease;
-
         static bool Prepare() => Options.Opts.Compactness != Options.CompactMode.Default;
-
-        static void Prefix(ref TextStyleSetting style)
-        {
-            if (style)
-                style.fontSize -= fontSizeDecrease;
-        }
-
-        static void Postfix(ref TextStyleSetting style)
-        {
-            if (style)
-                style.fontSize += fontSizeDecrease;
-        }
+        static void Prefix(TextStyleSetting style) => style.fontSize -= fontSizeDecrease;
+        static void Postfix(TextStyleSetting style) => style.fontSize += fontSizeDecrease;
     }
 
     [HarmonyPatch(typeof(HoverTextDrawer), nameof(HoverTextDrawer.NewLine))]
     class NewLineHeight_Patch
     {
         private static readonly int minHeight = Options.Opts.InfoCardSize.minHeight;
-
         static bool Prepare() => Options.Opts.Compactness != Options.CompactMode.Default;
-
-        static void Prefix(ref int min_height)
-        {
-            min_height = minHeight;
-        }
+        static void Prefix(ref int min_height) => min_height = minHeight;
     }
 
     [HarmonyPatch(typeof(HoverTextDrawer), nameof(HoverTextDrawer.DrawIcon), new Type[] { typeof(Sprite), typeof(Color), typeof(int), typeof(int)})]
     class MaxImageSize_Patch
     {
         private static readonly int maxImageSize = Options.Opts.InfoCardSize.maxImageSize;
-
         static bool Prepare() => Options.Opts.Compactness != Options.CompactMode.Default;
-
         static void Prefix(ref int image_size)
         {
             if (image_size > 16)
@@ -57,7 +39,7 @@ namespace BetterInfoCards
         private static readonly Vector2 border = Options.Opts.InfoCardSize.ShadowBarBorder;
         private static readonly float opacity = Options.Opts.InfoCardOpacity;
 
-        static void Prefix(ref HoverTextDrawer.Skin skin)
+        static void Prefix(HoverTextDrawer.Skin skin)
         {
             if(Options.Opts.Compactness != Options.CompactMode.Default)
                 skin.shadowBarBorder = border;
