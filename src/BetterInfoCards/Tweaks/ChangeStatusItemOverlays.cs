677d72d4d14bcd780421102418749f74cbbd1f01
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2021-05-23 15:38:45 -0700

    Faster method of preventing unreachable Status Items from being drawn
    Prevent drawing by modifying the status item's valid overlays, instead of postfixing and checking ID.

---
+++ src/BetterInfoCards/Tweaks/ChangeStatusItemOverlays.cs
@@ -0,0 +1,17 @@
+﻿using Database;
+using Harmony;
+
+namespace BetterInfoCards.Tweaks
+{
+    [HarmonyPatch(typeof(MiscStatusItems), nameof(MiscStatusItems.CreateStatusItems))]
+    class ChangeStatusItemOverlays
+    {
+        static void Postfix(MiscStatusItems __instance)
+        {
+            // Prevents the duplicate temp status from being drawn in the temp overlay.
+            __instance.OreTemp.status_overlays &= ~(int)StatusItem.StatusItemOverlays.Temperature;
+            // Prevent the unreachable status from being drawn on any overlay.
+            __instance.PickupableUnreachable.status_overlays = 0;
+        }
+    }
+}
