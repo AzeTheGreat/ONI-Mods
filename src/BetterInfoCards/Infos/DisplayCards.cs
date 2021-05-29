8f79be68df7b89a30a92082a3eaa781ade3a4dec
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2021-05-28 17:04:20 -0700

    Remove stopwatch performance testing

--- src/BetterInfoCards/Infos/DisplayCards.cs
+++ src/BetterInfoCards/Infos/DisplayCards.cs
@@ -1,22 +1,11 @@
 ﻿using BetterInfoCards.Util;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace BetterInfoCards
 {
     public class DisplayCards
     {
-        static Stopwatch one = new();
-        static Stopwatch two = new();
-        static Stopwatch three = new();
-        static Stopwatch four = new();
-        static Stopwatch five = new();
-        static Stopwatch dispCard = new();
-        static Stopwatch dispCard2 = new();
-
-        static int runs = 1;
-
         public List<DisplayCard> UpdateData(List<InfoCard> infoCards)
         {
             var displayCards = new List<DisplayCard>();
@@ -24,55 +13,18 @@ namespace BetterInfoCards
             if (infoCards == null || infoCards.Count <= 0)
                 return displayCards;
 
-            if(runs > 1000)
-            {
-                Debug.Log("5: " + five.ElapsedTicks / runs + " ticks");
-                Debug.Log("1: " + one.ElapsedTicks / runs + " ticks");
-                Debug.Log("2: " + two.ElapsedTicks / runs + " ticks");
-                Debug.Log("3: " + three.ElapsedTicks / runs + " ticks");
-                Debug.Log("4: " + four.ElapsedTicks / runs + " ticks");
-                Debug.Log("DC1: " + dispCard.ElapsedTicks / runs + " ticks");
-                Debug.Log("DC2: " + dispCard2.ElapsedTicks / runs + " ticks");
-                
-                
-                runs = 1;
-
-                one.Reset();    // 3323
-                two.Reset();    // 7600
-                five.Reset();
-                three.Reset();  // 1834
-                four.Reset();   // 4075
-                dispCard.Reset();
-                dispCard2.Reset(); 
-                
-                Debug.Log("RESET");
-            }
-
-            five.Start();
             var dSplit = infoCards.SplitByKey(card => card.GetTitleKey());
-            five.Stop();
-
-            one.Start();
             var dSplit2 = dSplit.SplitMany(cards => cards.SplitByKey(card => card.textInfos.Count));
-            one.Stop();
-
-            two.Start();
             var dSplit3 = GetTextKeySplits(dSplit2);
-            two.Stop();
 
-            three.Start();
             // Assumes each IC in the group has the same TIs (order doesn't matter).
             var dSplit4 = dSplit3.SplitMany(cards => cards.SplitBySplitters(
                 cards.First().textInfos.ToList(),
                 (group, ti) => ti.Value.SplitByTIDefs(group)));
-            three.Stop();
 
-            four.Start();
             foreach (var cards in dSplit4)
                 displayCards.Add(new DisplayCard(cards));
-            four.Stop();
 
-            runs++;
             return displayCards;
 
             // Assumes that the ICs in the same group have the same number of TIs.
