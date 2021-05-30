ad10a2b9db118b4d0e0bdd93c5c88418d5d5d4e9
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2021-05-30 15:54:45 -0700

    Remove unused code

--- src/BetterInfoCards/Infos/DisplayCard.cs
+++ src/BetterInfoCards/Infos/DisplayCard.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 namespace BetterInfoCards
 {
@@ -9,11 +8,6 @@ namespace BetterInfoCards
         private List<InfoCard> infoCards;
         private int visCardIndex;
         private InfoCard VisCard => infoCards[visCardIndex];
-
-        // The HoverTextScreen is initialized before CameraController
-        private float _minY = float.MaxValue;
-        private float MinY => _minY == float.MaxValue ?
-            _minY = -CameraController.Instance?.uiCamera.pixelRect.height / HoverTextScreen.Instance.GetComponentInParent<Canvas>().scaleFactor ?? float.MaxValue : _minY;
 
         public DisplayCard(List<InfoCard> infoCards)
         {
