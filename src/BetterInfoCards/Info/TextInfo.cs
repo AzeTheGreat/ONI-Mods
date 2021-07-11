dcbf1549243d6b561436fb1419a7c3e7c457d27e
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2021-07-10 18:13:40 -0700

    Remove unnecessary using
    This was an artifact of merging branches.

--- src/BetterInfoCards/Info/TextInfo.cs
+++ src/BetterInfoCards/Info/TextInfo.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Entry = HoverTextDrawer.Pool<UnityEngine.MonoBehaviour>.Entry;
 
 namespace BetterInfoCards
 {
