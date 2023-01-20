8a78629992400a2a9531960a7439148bab83907c
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-01-19 19:15:10 -0700

    Fix: Labels applied to incorrect buildings
    Allow filtering label application based on the type of logic ports that
    the target object has.  This removes unnecessary labels from the
    massage table, automatic dispenser, and spice grinder.
    
    Some objects have settings and logic ports, but it is not internally
    consistent to display the labels.  The intent is to apply labels to any
    settings that change what a port would output.  Labels should only be
    applied to input ports when the setting alters how they might be wired.
    This is why the filter settings are applied to inputs and outputs.
    Some objects will still be exceptions, necessitating a blocklist.

--- src/BetterLogicOverlay/LogicLabelSettings/ApplySettingsToDefs.cs
+++ src/BetterLogicOverlay/LogicLabelSettings/ApplySettingsToDefs.cs
@@ -2,6 +2,7 @@
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BetterLogicOverlay.LogicSettingDisplay
@@ -17,16 +18,32 @@ namespace BetterLogicOverlay.LogicSettingDisplay
 
                 // If there are no logic ports: skip this def.
                 // The LogicPorts fields are not reliable; some configs don't set them, but still yield a LogicPorts component.
-                if (!go.GetComponent<LogicPorts>() && !go.GetComponent<LogicGateBase>())
+                if (!go.TryGetComponent<LogicPorts>(out var logicPorts) & !go.TryGetComponent<LogicGateBase>(out var logicGateBase))
                     continue;
 
+                var hasInput = (logicPorts?.inputPortInfo?.Any() ?? false) || (logicGateBase?.inputPortOffsets?.Any() ?? false);
+                var hasOutput = (logicPorts?.outputPortInfo?.Any() ?? false) || (logicGateBase?.outputPortOffsets?.Any() ?? false);
+
                 // Add the correct setting component for the building.
-                foreach (var (building, setting) in buildingToLabelMap)
+                foreach (var (building, setting, filter) in buildingToLabelMap)
                 {
                     if (building != null && HasComponentOrDef(building, go))
                     {
-                        go.AddComponent(setting);
-                        break;
+                        var shouldAdd = filter switch
+                        {
+                            PortFilter.Input => hasInput,
+                            PortFilter.Output => hasOutput,
+                            PortFilter.InputOutput => hasInput || hasOutput,
+                            _ => throw new ArgumentException("Invalid PortFilter Enum"),
+                        };
+
+                        if (shouldAdd)
+                        {
+                            if(setting != null)
+                                go.AddComponent(setting);
+                                
+                            break;
+                        }
                     }
                 }
 
@@ -36,41 +53,50 @@ namespace BetterLogicOverlay.LogicSettingDisplay
 
         // Maps a component on the def to the correct type of setting to add.
         // Processed first to last, so interface fallbacks should be after specific implementations.
-        static readonly List<(Type building, Type setting)> buildingToLabelMap = new()
+        static readonly List<(Type building, Type setting, PortFilter filter)> buildingToLabelMap = new()
         {
+            // Blocklist
+            (typeof(SpiceGrinder.Def), null, PortFilter.Input),
+
             // IThresholdSwitch
-            (typeof(ConduitTemperatureSensor), typeof(ThresholdSwitchSetting.ConduitTemp)),
-            (typeof(LogicDiseaseSensor), typeof(ThresholdSwitchSetting.Germs)),
-            (typeof(ConduitDiseaseSensor), typeof(ThresholdSwitchSetting.Germs)),
-            (typeof(LogicCritterCountSensor), typeof(ThresholdSwitchSetting.CritterCount)),
-            (typeof(IThresholdSwitch), typeof(ThresholdSwitchSetting)),
+            (typeof(ConduitTemperatureSensor), typeof(ThresholdSwitchSetting.ConduitTemp), PortFilter.Output),
+            (typeof(LogicDiseaseSensor), typeof(ThresholdSwitchSetting.Germs), PortFilter.Output),
+            (typeof(ConduitDiseaseSensor), typeof(ThresholdSwitchSetting.Germs), PortFilter.Output),
+            (typeof(LogicCritterCountSensor), typeof(ThresholdSwitchSetting.CritterCount), PortFilter.Output),
+            (typeof(IThresholdSwitch), typeof(ThresholdSwitchSetting), PortFilter.Output),
 
             // ISliderControl
             // Applies to many buildings that shouldn't have labels, so whitelisting is the easiest approach.
-            (AccessTools.TypeByName("WirelessSignalReceiver"), typeof(SliderControlSetting.WirelessSignalRecieverSetting)),
-            (AccessTools.TypeByName("WirelessSignalEmitter"), typeof(SliderControlSetting.WirelessSignalEmitterSetting)),
-            (typeof(LogicGateFilter), typeof(SliderControlSetting)),
-            (typeof(LogicGateBuffer), typeof(SliderControlSetting)),
-
-            // General categories
-            (typeof(IActivationRangeTarget), typeof(ActivationRangeTargetSetting)),
-            (typeof(ILogicRibbonBitSelector), typeof(LogicRibbonBitSelectorSetting)),
-            (typeof(IUserControlledCapacity), typeof(UserControlledCapacitySetting)),
+            (AccessTools.TypeByName("WirelessSignalReceiver"), typeof(SliderControlSetting.WirelessSignalRecieverSetting), PortFilter.Output),
+            (AccessTools.TypeByName("WirelessSignalEmitter"), typeof(SliderControlSetting.WirelessSignalEmitterSetting), PortFilter.Input),
+            (typeof(LogicGateFilter), typeof(SliderControlSetting), PortFilter.Output),
+            (typeof(LogicGateBuffer), typeof(SliderControlSetting), PortFilter.Output),
 
             // Specific buildings
-            (typeof(Filterable), typeof(ConduitElementSensorSetting)),
-            (typeof(LogicElementSensor), typeof(LogicElementSensorSetting)),
-            (typeof(LogicCounter), typeof(LogicCounterSetting)),
-            (typeof(LogicTimerSensor), typeof(LogicTimerSensorSetting)),
-            (typeof(LogicTimeOfDaySensor), typeof(LogicTimeOfDaySensorSetting)),
-            (typeof(LogicBroadcaster), typeof(LogicBroadcasterSetting)),
-            (typeof(LogicBroadcastReceiver), typeof(LogicBroadcasterSetting.Receiver)),
-            (typeof(CometDetector.Def), typeof(CometDetectorSetting)),
-            (typeof(ClusterCometDetector.Def), typeof(ClusterCometDetectorSetting)),
+            (typeof(Filterable), typeof(ConduitElementSensorSetting), PortFilter.InputOutput),
+            (typeof(LogicElementSensor), typeof(LogicElementSensorSetting), PortFilter.Output),
+            (typeof(LogicCounter), typeof(LogicCounterSetting), PortFilter.Output),
+            (typeof(LogicTimerSensor), typeof(LogicTimerSensorSetting), PortFilter.Output),
+            (typeof(LogicTimeOfDaySensor), typeof(LogicTimeOfDaySensorSetting), PortFilter.Output),
+            (typeof(LogicBroadcaster), typeof(LogicBroadcasterSetting), PortFilter.Input),
+            (typeof(LogicBroadcastReceiver), typeof(LogicBroadcasterSetting.Receiver), PortFilter.Output),
+            (typeof(CometDetector.Def), typeof(CometDetectorSetting), PortFilter.Output),
+            (typeof(ClusterCometDetector.Def), typeof(ClusterCometDetectorSetting), PortFilter.Output),
 
             // Specific mod buildings
-            (typeof(TreeFilterable), typeof(SolidElementSetting))
+            (typeof(TreeFilterable), typeof(SolidElementSetting), PortFilter.InputOutput),
+
+            // General categories
+            (typeof(IActivationRangeTarget), typeof(ActivationRangeTargetSetting), PortFilter.Output),
+            (typeof(ILogicRibbonBitSelector), typeof(LogicRibbonBitSelectorSetting), PortFilter.Output),
+            (typeof(IUserControlledCapacity), typeof(UserControlledCapacitySetting), PortFilter.Output)
         };
 
+        private enum PortFilter
+        {
+            Input,
+            Output,
+            InputOutput
+        }
     }
 }
