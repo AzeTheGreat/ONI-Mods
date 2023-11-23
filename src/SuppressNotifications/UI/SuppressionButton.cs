7d6fbd51dcf5380769b54a7f1f7f99e4e193361e
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-11-23 14:29:07 -0700

    Style: Update conventions and formatting
    Reorganize code to improve logical flow.  Slightly abstract button
    creation.  Use shorthand and update conventions where applicable.

--- src/SuppressNotifications/UI/SuppressionButton.cs
+++ src/SuppressNotifications/UI/SuppressionButton.cs
@@ -1,69 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using static STRINGS.MISC;
 
 namespace SuppressNotifications
 {
     public class SuppressionButton : KMonoBehaviour
     {
-        [MyCmpAdd]
-        internal StatusItemsSuppressedComp statusItemsSuppressedComp;
-        [MyCmpAdd]
-        internal NotificationsSuppressedComp notificationsSuppressedComp;
+        [MyCmpAdd] private StatusItemsSuppressedComp statusItemsSuppressedComp;
+        [MyCmpAdd] private NotificationsSuppressedComp notificationsSuppressedComp;
 
         public override void OnPrefabInit()
         {
-            Subscribe((int)GameHashes.RefreshUserMenu, (object data) => OnRefreshUserMenu());
+            Subscribe((int)GameHashes.RefreshUserMenu, (_) => OnRefreshUserMenu());
         }
 
         private void OnRefreshUserMenu()
         {
-            // For vanilla and SO, must explicitly set the right action since this enum is different.
-            Enum.TryParse(nameof(Action.NumActions), out Action action);
-
             if (AreSuppressable())
-                Game.Instance.userMenu.AddButton(gameObject, new KIconButtonMenu.ButtonInfo("action_building_disabled", MYSTRINGS.SUPPRESSBUTTON.NAME, new System.Action(OnSuppressClick), action, tooltipText: GetSuppressableString()));
+                AddButton(MYSTRINGS.SUPPRESSBUTTON.NAME, OnSuppressClick, GetSuppressableString);
             else if (AreSuppressed())
-                Game.Instance.userMenu.AddButton(gameObject, new KIconButtonMenu.ButtonInfo("action_building_disabled", MYSTRINGS.CLEARBUTTON.NAME, new System.Action(OnClearClick), action, tooltipText: GetSuppressedString()));
+                AddButton(MYSTRINGS.CLEARBUTTON.NAME, OnClearClick, GetSuppressedString);
+
+            void AddButton(string text, System.Action onClick, Func<string> getTooltip)
+            {
+                // For vanilla and SO, must explicitly set the right action since this enum is different.
+                Enum.TryParse(nameof(Action.NumActions), out Action action);
+
+                Game.Instance.userMenu.AddButton(gameObject, new KIconButtonMenu.ButtonInfo("action_building_disabled", text, onClick, action, tooltipText: getTooltip()));
+            }
         }
 
-        internal virtual bool AreSuppressable()
-        {
-            return statusItemsSuppressedComp.GetSuppressableStatusItems().Any() || notificationsSuppressedComp.GetSuppressableNotifications().Any();
-        }
-
-        internal virtual bool AreSuppressed()
-        {
-            return statusItemsSuppressedComp.suppressedStatusItemTitles.Any() || notificationsSuppressedComp.suppressedNotifications.Any();
-        }
-
-        internal virtual string GetSuppressableString()
-        {
-            return MYSTRINGS.SUPPRESSBUTTON.TOOLTIP + "\n"
-                    + GetStatusItemListText(statusItemsSuppressedComp.GetSuppressableStatusItems())
-                    + GetNotificationListText(notificationsSuppressedComp.GetSuppressableNotifications());
-        }
-
-        internal virtual string GetSuppressedString()
-        {
-            return MYSTRINGS.CLEARBUTTON.TOOLTIP + "\n"
-                    + GetStatusItemListText(statusItemsSuppressedComp.suppressedStatusItemTitles)
-                    + GetNotificationListText(notificationsSuppressedComp.suppressedNotifications);
-        }
+        internal virtual bool AreSuppressable() => 
+            statusItemsSuppressedComp.GetSuppressableStatusItems().Any() || notificationsSuppressedComp.GetSuppressableNotifications().Any();
+        internal virtual bool AreSuppressed() => 
+            statusItemsSuppressedComp.suppressedStatusItemTitles.Any() || notificationsSuppressedComp.suppressedNotifications.Any();
 
         internal virtual void OnSuppressClick()
         {
             notificationsSuppressedComp.SuppressNotifications();
             statusItemsSuppressedComp.SuppressStatusItems();
-            Game.Instance.userMenu.Refresh(base.gameObject);
+            Game.Instance.userMenu.Refresh(gameObject);
         }
 
         internal virtual void OnClearClick()
         {
             notificationsSuppressedComp.UnsupressNotifications();
             statusItemsSuppressedComp.UnsuppressStatusItems();
-            Game.Instance.userMenu.Refresh(base.gameObject);
+            Game.Instance.userMenu.Refresh(gameObject);
+        }
+
+        internal virtual string GetSuppressableString()
+        {
+            return MYSTRINGS.SUPPRESSBUTTON.TOOLTIP + Environment.NewLine
+                    + GetStatusItemListText(statusItemsSuppressedComp.GetSuppressableStatusItems())
+                    + GetNotificationListText(notificationsSuppressedComp.GetSuppressableNotifications());
+        }
+
+        internal virtual string GetSuppressedString()
+        {
+            return MYSTRINGS.CLEARBUTTON.TOOLTIP + Environment.NewLine
+                    + GetStatusItemListText(statusItemsSuppressedComp.suppressedStatusItemTitles)
+                    + GetNotificationListText(notificationsSuppressedComp.suppressedNotifications);
         }
 
         private string GetStatusItemListText(List<StatusItem> statusItems) => GetStatusItemListText(statusItems.Select(x => x.Name).ToList());
@@ -78,11 +75,8 @@ namespace SuppressNotifications
                 return string.Empty;
 
             string text = Environment.NewLine + label + Environment.NewLine;
-
             foreach (var item in items)
-            {
-                text = text + item + "\n";
-            }
+                text += item + Environment.NewLine;
 
             return text;
         }
