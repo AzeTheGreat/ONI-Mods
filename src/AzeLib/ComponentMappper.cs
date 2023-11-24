8dbfb6f0e67700f63e0eaf31d4d4fc2578e806ed
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-11-24 13:32:51 -0700

    Refac: Move component mapping logic into lib
    Extract component mapping logic from Better Logic Overlay to the main
    lib.  This is a useful pattern that other mods can make use of.

---
+++ src/AzeLib/ComponentMappper.cs
@@ -0,0 +1,40 @@
+﻿using AzeLib.Extensions;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AzeLib
+{
+    public class ComponentMapper<T>
+    {
+        private readonly List<(Type flagCmp, Type addCmp, T filter)> map;
+
+        /// <summary>
+        /// Maps detected components to new components that will be added.
+        /// Processed first to last, so interface fallbacks should be after specific implementations.
+        /// </summary>
+        public ComponentMapper(List<(Type flagCmp, Type addCmp, T filter)> map) => this.map = map;
+
+        /// <summary>
+        /// Apply component map to GO, adding new components if applicable.
+        /// </summary>
+        /// <param name="go">The GameObject to work on</param>
+        /// <param name="shouldAdd">Function uses filter to determine if the new component should be added to the GO</param>
+        public void ApplyMap(GameObject go, Func<T, bool> shouldAdd)
+        {
+            var typeToAdd = GetTypeToAdd(go, shouldAdd);
+            if (typeToAdd != null)
+                go.AddComponent(typeToAdd);
+        }
+
+        private Type GetTypeToAdd(GameObject go, Func<T, bool> shouldAdd)
+        {
+            foreach (var (flagCmp, addCmp, filter) in map)
+                if (flagCmp != null && HasComponentOrDef(flagCmp, go) && shouldAdd(filter))
+                    return addCmp;
+            return null;
+
+            bool HasComponentOrDef(Type cmpOrDef, GameObject go) => go.GetComponent(cmpOrDef) ?? go.GetDef(cmpOrDef) != null;
+        }
+    }
+}
