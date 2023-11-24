0257a2f2a5cdfeeb2556a18ed53ddf4d2ce5e2aa
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-11-24 14:04:45 -0700

    Refac: Generalize suppression button application
    Use component mapping to apply suppression buttons to prefabs instead
    of writing bespoke patches for each new button.
    
    Working with non-building prefabs requires null checking the
    StateMachineController since it may not be present.

--- src/AzeLib/ComponentMappper.cs
+++ src/AzeLib/ComponentMappper.cs
@@ -1,10 +1,21 @@
 ﻿using AzeLib.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace AzeLib
 {
+    public class ComponentMapper : ComponentMapper<object>
+    {
+        /// <inheritdoc cref="ComponentMapper{T}"/>
+        public ComponentMapper(List<(Type flagCmp, Type addCmp)> map) : base(map.Select(x => (x.flagCmp, x.addCmp, (object)null)).ToList())
+        { }
+
+        /// <inheritdoc cref="ComponentMapper{T}.ApplyMap(GameObject, Func{T, bool})"/>
+        public void ApplyMap(GameObject go) => ApplyMap(go, _ => true);
+    }
+
     public class ComponentMapper<T>
     {
         private readonly List<(Type flagCmp, Type addCmp, T filter)> map;
---
+++ src/SuppressNotifications/UI/ApplyButtonsToPrefabs.cs
@@ -0,0 +1,24 @@
+﻿using AzeLib;
+using HarmonyLib;
+using SuppressNotifications;
+
+namespace BetterLogicOverlay.LogicSettingDisplay
+{
+    [HarmonyPatch(typeof(Assets), nameof(Assets.CreatePrefabs))]
+    class ApplySettingsToDefs
+    {
+        static void Postfix()
+        {
+            var cmpMap = new ComponentMapper(new()
+            {
+                (typeof(Uprootable), typeof(PlantSuppressionButton)),
+                (typeof(MinionBrain), typeof(MinionSuppressionButton)),
+                (typeof(Capturable), typeof(CritterSuppressionButton)),
+                (typeof(BuildingComplete), typeof(BuildingSuppressionButton))
+            });
+
+            foreach (var prefab in Assets.Prefabs)
+                cmpMap.ApplyMap(prefab.gameObject);
+        }
+    }
+}
