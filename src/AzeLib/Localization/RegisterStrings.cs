594d99c2c1cb7a9911967eadf595b8db8aad729e
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-12-20 19:11:59 -0700

    Refac: Merge string registration
    Move RegisterFieldlessStrings into RegisterStrings.
    
    This reduces duplicate code and will marginally improve load times by
    reducing the number of patches and only loading translations once.

--- src/AzeLib/Localization/RegisterStrings.cs
+++ src/AzeLib/Localization/RegisterStrings.cs
@@ -10,45 +10,83 @@ namespace AzeLib
 
     }
 
+    public abstract class AFieldlessStrings<T> : SingletonBase<T>, IAFieldlessStrings where T : AFieldlessStrings<T>
+    {
+
+    }
+
+    interface IAFieldlessStrings
+    {
+
+    }
+
     [HarmonyPatch(typeof(Localization), nameof(Localization.Initialize))]
     static class RegisterStrings
     {
         private static List<Type> locStringRoots;
+        private static List<Type> fieldlessStringRoots;
+        private static List<IAFieldlessStrings> fieldlessStrings;
 
         // Only patch if the assembly actually has anything to translate.
         static bool Prepare()
         {
             locStringRoots = ReflectionHelpers.GetChildTypesOfGenericType(typeof(AStrings<>)).ToList();
-            return locStringRoots.Count >= 1;
+
+            fieldlessStringRoots = ReflectionHelpers.GetChildTypesOfGenericType(typeof(AFieldlessStrings<>)).ToList();
+            fieldlessStrings = fieldlessStringRoots.Select(SingletonHelper<IAFieldlessStrings>.GetInstance).ToList();
+
+            return locStringRoots.Any() || fieldlessStringRoots.Any();
         }
 
         static void Postfix()
         {
-            if (locStringRoots.FirstOrDefault() is Type rootType)
+            var translationsLoaded = AzeLocalization.TryLoadTranslations(out var translations);
+            var rootType = locStringRoots.FirstOrDefault();
+
+            if (rootType != null)
             {
                 // Manually add the assembly, instead of calling Localization.RegisterForTranslation, to avoid the pointless CreateLocStringKeys call.
                 Localization.AddAssembly(rootType.Namespace, rootType.Assembly);
 
-                SetLocStringFields();
-                SetStringsDBEntries();
-                AzeLocalization.GeneratePOTemplate(rootType);
+                if (translationsLoaded)
+                    SetLocStringFields(translations);
+                SetStringsDBEntries(locStringRoots);
             }
+
+            // If the default language is used, there will be no .po file to load.
+            // Instead, register based off the English defaults in these types.
+            if (Localization.GetSelectedLanguageType() == Localization.SelectedLanguageType.None)
+                SetStringsDBEntries(fieldlessStringRoots);
+            else if (translationsLoaded)
+                SetFieldlessStringsDBEntries(translations);
+
+            AzeLocalization.GeneratePOTemplate(rootType);
         }
 
         // Must manually load from "translations" because files in "strings" are always overloaded, even if they don't match the current locale.
-        private static void SetLocStringFields()
-        {
-            if (AzeLocalization.TryLoadTranslations(out var translations))
-                Localization.OverloadStrings(translations);
-        }
+        private static void SetLocStringFields(Dictionary<string, string> translations) => Localization.OverloadStrings(translations);
 
         // When strings are translated, the value of the LocString field is set, but the strings are never re-registered.
         // Thus any uses of Strings.Get return the untranslated value.
         // This calls LocString.CreateLocStringKeys to re-register the strings.
-        static void SetStringsDBEntries()
+        private static void SetStringsDBEntries(List<Type> types)
         {
-            foreach (var type in locStringRoots)
+            foreach (var type in types)
                 LocString.CreateLocStringKeys(type, AzeStrings.GetParentPath(type));
+        }
+
+        private static void SetFieldlessStringsDBEntries(Dictionary<string, string> translations)
+        {
+            foreach (var type in fieldlessStringRoots)
+            {
+                var targetType = type.Name + ".";
+                var translationsForType = translations.Where(kvp => kvp.Key.Contains(targetType));
+                foreach (var kvp in translationsForType)
+                {
+                    var partialKey = kvp.Key.Substring(kvp.Key.IndexOf(targetType) + targetType.Length);
+                    Strings.Add(AzeStrings.GetFullKey(type, partialKey), kvp.Value);
+                }
+            }
         }
     }
 }
