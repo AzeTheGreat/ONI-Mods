dfe1b447f37fd4800636483b64bfdf008967bfc6
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-12-22 10:19:54 -0700

    Feat: Custom POT Generator
    Re-implement Klei's POT generator.  Remove the useless comments left on
    each line and fix Klei's dictionary concat to actually work.
    
    This could have been done through patches or reverse patches, but
    enough modifications are required that it is simpler to fully redefine.
    This will only be used for debug builds and is standalone code, so this
    isn't a terrible approach.
    
    This is largely copied from Klei's decompiled code and has been renamed
    and broken into methods for clarity.

--- src/AzeLib/Localization/AzeLocalization.cs
+++ src/AzeLib/Localization/AzeLocalization.cs
@@ -32,7 +32,7 @@ namespace AzeLib
             // There's really no way to access MSBuild properties in C#.
             // This is the best we get: up three directories to get to ONI-Mods, then to the Translations folder.
             var transDir = Path.GetFullPath(Path.Combine(GetCallerPath(), "..", "..", "..", "..", TranslationFolder, rootType.Namespace));
-            Localization.GenerateStringsTemplate(rootType, transDir);
+            POTemplateGenerator.GeneratePOT(rootType, transDir);
 
             // This uses Roslyn to inject the path of this .cs file at compile time.
             static string GetCallerPath([CallerFilePath] string path = null) => path;
---
+++ src/AzeLib/Localization/POTemplateGenerator.cs
@@ -0,0 +1,95 @@
+﻿#if DEBUG
+
+using Klei;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AzeLib
+{
+    internal class POTemplateGenerator
+    {
+        internal static void GeneratePOT(Type locStringRoot, string outputDir)
+        {
+            outputDir = FileSystem.Normalize(outputDir);
+            if (!FileUtil.CreateDirectory(outputDir, 5))
+                return;
+
+            var outputPath = FileSystem.Normalize(Path.Combine(outputDir, $"{locStringRoot.Namespace.ToLower()}_template.pot"));
+            GeneratePOT(locStringRoot.Namespace, Assembly.GetAssembly(locStringRoot), outputPath, null);
+        }
+
+        private static void GeneratePOT(string lsNamespace, Assembly lsAssembly, string outputDir, Dictionary<string, object> extraStringsTree)
+        {
+            using (var sw = new StreamWriter(outputDir, false, new UTF8Encoding(false)))
+            {
+                WriteHeader(sw);
+                WritePOT(lsNamespace, sw, GetStringsTree());
+            }
+
+            Debug.Log("Generated AzePOT at " + outputDir);
+
+            static void WriteHeader(StreamWriter sw)
+            {
+                sw.WriteLine("msgid \"\"");
+                sw.WriteLine("msgstr \"\"");
+                sw.WriteLine("\"Application: Oxygen Not Included\"");
+                sw.WriteLine("\"POT Version: 2.0\"");
+                sw.WriteLine();
+            }
+
+            Dictionary<string, object> GetStringsTree()
+            {
+                var stringsTree = new Dictionary<string, object>();
+                foreach (var rootType in Localization.CollectLocStringTreeRoots(lsNamespace, lsAssembly))
+                {
+                    var rootStringsTree = Localization.MakeRuntimeLocStringTree(rootType);
+                    if (rootStringsTree.Any())
+                        stringsTree[rootType.Name] = rootStringsTree;
+                }
+
+                if (extraStringsTree != null)
+                    stringsTree = stringsTree.Concat(extraStringsTree).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+                return stringsTree;
+            }
+        }
+
+        private static void WritePOT(string path, StreamWriter sw, Dictionary<string, object> stringsTree)
+        {
+            foreach (var (key, value) in stringsTree.OrderBy(k => k.Key))
+            {
+                var fullKey = path + "." + key;
+
+                if (value is string defaultTranslation)
+                    WriteEntry(sw, fullKey, FixupString(defaultTranslation));
+                else
+                    WritePOT(fullKey, sw, value as Dictionary<string, object>);
+            }
+
+            static void WriteEntry(StreamWriter sw, string fullKey, string defaultTranslation)
+            {
+                sw.WriteLine("msgctxt \"{0}\"", fullKey);
+                sw.WriteLine("msgid \"" + defaultTranslation + "\"");
+                sw.WriteLine("msgstr \"\"");
+                sw.WriteLine();
+            }
+
+            static string FixupString(string str)
+            {
+                return str.Replace("\\", "\\\\")
+                    .Replace("\"", "\\\"")
+                    .Replace("\n", "\\n")
+                    .Replace("’", "'")
+                    .Replace("“", "\\\"")
+                    .Replace("”", "\\\"")
+                    .Replace("…", "...");
+            }
+        }
+    }
+}
+
+#endif
