0b2a8edbd4398a8adc4b73a7f8b973a6f8898556
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2024-02-18 13:54:14 -0700

    Feat: Add label so users know how to rebind

--- src/BuildMenuSearchHotkey/Util/Options.cs
+++ src/BuildMenuSearchHotkey/Util/Options.cs
@@ -1,10 +1,12 @@
 ﻿using AzeLib;
+using Newtonsoft.Json;
 using PeterHan.PLib.Options;
 
 namespace BuildMenuSearchHotkey
 {
     public class Options : BaseOptions<Options>
     {
+        [JsonIgnore][Option] public LocText PointToKeybindMenuLabel { get; set; }
         [Option] public bool HotkeyWorksWhenBuildMenuClosed { get; set; } = true;
     }
 }
--- src/BuildMenuSearchHotkey/Util/STRINGS.cs
+++ src/BuildMenuSearchHotkey/Util/STRINGS.cs
@@ -17,5 +17,11 @@ namespace BuildMenuSearchHotkey
             public static LocString NAME = "Open Menu if Closed";
             public static LocString TOOLTIP = $"When true, pressing the {MYSTRINGS.BUILDSEARCHHOTKEY.NAME} hotkey will open the first available category and then select search.";
         }
+
+        public class POINTTOKEYBINDMENULABEL
+        {
+            public static LocString NAME = $"<i>Configure binding in Controls > Mods.</i>";
+            public static LocString TOOLTIP = "Default binding is Ctrl + E.";
+        }
     }
 }
