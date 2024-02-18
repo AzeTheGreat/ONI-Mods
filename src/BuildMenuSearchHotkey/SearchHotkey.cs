8ffc5ad4b9fe9df17a03bff10205f04df0d3e4e2
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2024-02-18 13:18:17 -0700

    Feat: Define a hotkey to select build menu search

---
+++ src/BuildMenuSearchHotkey/SearchHotkey.cs
@@ -0,0 +1,32 @@
+﻿using AzeLib.Attributes;
+using HarmonyLib;
+using PeterHan.PLib.Actions;
+
+namespace BuildMenuSearchHotkey
+{
+    public class SearchHotkey
+    {
+        private static Action action;
+
+        [OnLoad]
+        public static void InitAction()
+        {
+            var manager = new PActionManager();
+            var pAction = manager.CreateAction("AzeTestBuildSearchHotkey", "Build Search Hotkey", new(KKeyCode.E, Modifier.Ctrl));
+            action = pAction.GetKAction();
+        }
+
+        [HarmonyPatch(typeof(PlanScreen), nameof(PlanScreen.OnKeyDown))]
+        class ListenForAction
+        {
+            static void Postfix(KButtonEvent e)
+            {
+                if (PlanScreen.Instance.activeCategoryInfo == null)
+                    return;
+
+                if (e.TryConsume(action))
+                    BuildingGroupScreen.Instance.inputField.Select();
+            }
+        }
+    }
+}
