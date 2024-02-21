6a967560f438c20975917a4c7eac0b4d20c70609
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2024-02-20 17:34:25 -0700

    Feat: Call custom backspace when ctrl held
    Transpile TMP InputFields to check if ctrl is held before calling the
    Backspace method.  If ctrl is held, call custom CtrlBackspace method.
    
    Publicise TMP to simplify member access.

---
+++ src/DeleteFullWord/DetectCtrlModifier.cs
@@ -0,0 +1,37 @@
+﻿using AzeLib.Extensions;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TMPro;
+using UnityEngine;
+
+namespace DeleteFullWord;
+
+[HarmonyPatch(typeof(TMP_InputField), nameof(TMP_InputField.KeyPressed))]
+public class DetectCtrlModifier
+{
+    // Wrap the Backspace call so that Ctrl can be checked for and handled.
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+    {
+        return codes.Manipulator(
+            i => i.Calls(AccessTools.Method(typeof(TMP_InputField), nameof(TMP_InputField.Backspace))),
+            i => [
+                new CodeInstruction(OpCodes.Ldarg_1),
+                CodeInstruction.Call(typeof(DetectCtrlModifier), nameof(Wrapper))
+            ]);
+    }
+
+    private static void Wrapper(TMP_InputField instance, Event evt)
+    {
+        var isControlPressed = (evt.modifiers & EventModifiers.Control) > EventModifiers.None;
+        if (isControlPressed)
+            CtrlBackspace();
+        else
+            instance.Backspace();
+    }
+
+    private static void CtrlBackspace()
+    {
+
+    }
+}
