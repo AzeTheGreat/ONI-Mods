95b50f1d87f648775586ad67dabe657d5fc27e3f
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2025-06-08 13:48:27 -0700

    Feat: Hide corner decors

--- src/CleanFloors/RemoveDecors.cs
+++ src/CleanFloors/RemoveDecors.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,10 +8,17 @@ namespace CleanFloors
     [HarmonyPatch(typeof(Assets), nameof(Assets.OnPrefabInit))]
     public class RemoveDecors
     {
-        private static readonly List<string> decorsToRemove = new List<string> { "tops" };
+        static bool removeOutsideCorners = true;
+        static bool removeInsideCorners = true;
 
         static void Postfix()
         {
+            List<string> decorsToRemove = [
+                "tops",
+                ..(removeOutsideCorners ? [ "outside_bl_corner", "outside_br_corner", "outside_tl_corner", "outside_tr_corner" ] : Array.Empty<string>()),
+                ..(removeInsideCorners ? [ "inside_bl_corner", "inside_br_corner", "inside_tl_corner", "inside_tr_corner" ] : Array.Empty<string>())
+            ];
+
             foreach (var decorInfo in Assets.BlockTileDecorInfos)
             {
                 decorInfo.decor = decorInfo.decor.Select(decor =>
