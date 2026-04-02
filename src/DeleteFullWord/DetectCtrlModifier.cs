8fe2fdaa64e55393dbd3fd2c64604b379eead3da
Peter Crabtree <peter.crabtree@gmail.com>
2026-04-01 22:34:22 -0400

    Fix Text Input Hotkeys for latest TMP input field

--- src/DeleteFullWord/DetectCtrlModifier.cs
+++ src/DeleteFullWord/DetectCtrlModifier.cs
@@ -1,5 +1,6 @@
-﻿using AzeLib.Extensions;
+using AzeLib.Extensions;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using TMPro;
@@ -10,6 +11,35 @@ namespace DeleteFullWord;
 [HarmonyPatch(typeof(TMP_InputField), nameof(TMP_InputField.KeyPressed))]
 public class DetectCtrlModifier
 {
+    private static readonly char[] WordSeparators = [' ', '.', ',', '\t', '\r', '\n'];
+    private static readonly Func<TMP_InputField, int> FindPreviousWordBegin;
+
+    static DetectCtrlModifier()
+    {
+        // TMP renamed this private helper from FindtPrevWordBegin to FindPrevWordBegin in newer builds.
+        var method =
+            AccessTools.Method(typeof(TMP_InputField), "FindPrevWordBegin") ??
+            AccessTools.Method(typeof(TMP_InputField), "FindtPrevWordBegin");
+
+        if (method != null)
+        {
+            FindPreviousWordBegin = AccessTools.MethodDelegate<Func<TMP_InputField, int>>(method, null, false, Type.EmptyTypes);
+            return;
+        }
+
+        // Match the legacy helper behavior if neither private method name is available, rather than crashing.
+        FindPreviousWordBegin = instance =>
+        {
+            var text = instance.text ?? string.Empty;
+            var searchPosition = Math.Min(instance.stringSelectPositionInternal, text.Length);
+            if (searchPosition - 2 < 0)
+                return 0;
+
+            var separatorIndex = text.LastIndexOfAny(WordSeparators, searchPosition - 2);
+            return separatorIndex == -1 ? 0 : separatorIndex + 1;
+        };
+    }
+
     // Wrap the Backspace call so that Ctrl can be checked for and handled.
     static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
     {
@@ -27,7 +57,7 @@ public class DetectCtrlModifier
         if (isControlPressed)
         {
             var isShiftPressed = (evt.modifiers & EventModifiers.Shift) > EventModifiers.None;
-            var charsToRemove = isShiftPressed ? instance.stringPositionInternal : instance.stringPositionInternal - instance.FindtPrevWordBegin();
+            var charsToRemove = isShiftPressed ? instance.stringPositionInternal : instance.stringPositionInternal - FindPreviousWordBegin(instance);
 
             if (charsToRemove > 0)
                 ModifiedBackspace.MultiBackspace(instance, charsToRemove);
